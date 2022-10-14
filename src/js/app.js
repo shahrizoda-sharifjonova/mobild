4e3ab351e3f6d1fd214f03e600f50d2572927f72
shahrizoda <shahrizodadeveloping@gmail.com>
2022-10-15 00:15:39 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,33 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const img = document.querySelector('.intro__img');
+const right = document.querySelector('.intro__right');
+
+window.addEventListener('resize', (e)=>{
+    if(window.innerWidth <= 576){
+        const h = img.clientHeight / 2 + 10
+        right.style.paddingTop = `${h}px`
+        console.log(right )
+    }
+    else if(window.innerWidth > 576 && window.innerWidth <= 1400){
+        right.style.paddingTop = '166px'
+    }
+    else{
+        right.style.paddingTop = '0'
+    }
+})
+window.addEventListener('load', (e)=>{
+    if(window.innerWidth <= 576){
+        const h = img.clientHeight / 2 + 10
+        right.style.paddingTop = `${h}px`
+        console.log(right )
+    }
+    else if(window.innerWidth > 576 && window.innerWidth <= 1400){
+        right.style.paddingTop = '166px'
+    }
+    else{
+        right.style.paddingTop = '0'
+    }
+})
